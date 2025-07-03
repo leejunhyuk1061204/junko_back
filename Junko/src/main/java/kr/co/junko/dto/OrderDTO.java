8723c910eeb8d1a6957ec6843eefc37971fbd618
Leejunhyuk <wnsgur0813@gmail.com>
2025-07-04 00:48:40 +0900

    git test용 orderDTO 생성

---
+++ Junko/src/main/java/kr/co/junko/dto/OrderDTO.java
@@ -0,0 +1,18 @@
+package kr.co.junko.dto;
+
+import java.time.LocalDate;
+
+import lombok.Data;
+
+@Data
+public class OrderDTO {
+
+	private int order_idx;
+	private int custom_idx;
+	private int warehouse_idx;
+	private LocalDate reg_date;
+	private int user_idx;
+	private String status;
+	private boolean delete;
+	
+}
