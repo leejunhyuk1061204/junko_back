52d25798b4d4f97d36292058671df24546a72396
LeeJunHyuk <junhyuk@naver.com>
2025-07-03 15:41:03 +0900

    첫번째 커밋

---
+++ Junko/src/main/java/kr/co/junko/JunkoApplication.java
@@ -0,0 +1,13 @@
+package kr.co.junko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunkoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunkoApplication.class, args);
+	}
+
+}
---
+++ Junko/src/test/java/kr/co/junko/JunkoApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.junko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunkoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
