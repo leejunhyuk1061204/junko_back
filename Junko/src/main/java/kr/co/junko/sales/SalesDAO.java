e99cbfddfa4577c8a416144fd5dcfd32f41f3128
Leejunhyuk <wnsgur0813@gmail.com>
2025-07-07 11:50:06 +0900

    sales CRUD

---
+++ Junko/src/main/java/kr/co/junko/sales/SalesDAO.java
@@ -0,0 +1,23 @@
+package kr.co.junko.sales;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import kr.co.junko.dto.SalesDTO;
+
+@Mapper
+public interface SalesDAO {
+
+	int salesInsert(SalesDTO dto);
+
+	int salesUpdate(SalesDTO dto);
+
+	List<SalesDTO> salesList(Map<String, Object> param);
+
+	int salesListTotalPage(Map<String, Object> param);
+
+	int salesDel(int idx);
+
+}
