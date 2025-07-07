a283ec17226594e0d6a38bb3c5511e23f73d4f03
kim_kyoung_whoe <kw72622@naver.com>
2025-07-07 21:33:38 +0900

    수금 지급  기능 완료

--- Junko/src/main/java/kr/co/junko/collectonAndPayment/CollectionAndPaymentDAO.java
+++ Junko/src/main/java/kr/co/junko/collectonAndPayment/CollectionAndPaymentDAO.java
@@ -1,11 +1,16 @@
 package kr.co.junko.collectonAndPayment;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import kr.co.junko.dto.CollectionAndPaymentLogDTO;
 import kr.co.junko.dto.CollectionAndPaymentRequestDTO;
 import kr.co.junko.dto.CollectionAndPaymentResponseDTO;
+import kr.co.junko.dto.CustomDTO;
+import kr.co.junko.dto.FileDTO;
+import kr.co.junko.dto.LinkedItemDTO;
 
 @Mapper
 public interface CollectionAndPaymentDAO {
@@ -20,4 +25,25 @@ public interface CollectionAndPaymentDAO {
 
 	int capDel(int cap_idx);
 
+	List<CustomDTO> capCustom();
+
+	List<CustomDTO> capCustomList();
+
+	LinkedItemDTO getEntryList();
+
+	LinkedItemDTO getSettlementList();
+
+	LinkedItemDTO getInvoiceList();
+
+	void insertFile(FileDTO dto);
+
+	FileDTO getFileByIdx(int file_idx);
+
+	void deleteFile(int file_idx);
+
+	List<CollectionAndPaymentLogDTO> getLogsByCapIdx(int cap_idx);
+
+	void insertLog(CollectionAndPaymentLogDTO dto);
+	
+	
 }
