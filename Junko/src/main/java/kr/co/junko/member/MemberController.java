f75860b0a3dc267d96b3d874a570c87f3c504b52
LeeJunHyuk <junhyuk@naver.com>
2025-07-11 12:43:13 +0900

    멤버 크로스오리진

--- Junko/src/main/java/kr/co/junko/member/MemberController.java
+++ Junko/src/main/java/kr/co/junko/member/MemberController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +18,7 @@ import kr.co.junko.util.Jwt;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+@CrossOrigin
 @RestController
 @Slf4j
 @RequiredArgsConstructor
