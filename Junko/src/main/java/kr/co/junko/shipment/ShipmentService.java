deb24335b3ee157e96cb2fd80d82d18765e1c82c
Leejunhyuk <wnsgur0813@gmail.com>
2025-07-20 22:27:08 +0900

    출고 완료

--- Junko/src/main/java/kr/co/junko/shipment/ShipmentService.java
+++ Junko/src/main/java/kr/co/junko/shipment/ShipmentService.java
@@ -44,21 +44,36 @@ public class ShipmentService {
 	}
 
 	public Map<String, Object> shipmentList(Map<String, Object> param) {
-		int cnt = 10;
-		int offset = ((int)param.get("page")-1)*cnt;
-		param.put("cnt", cnt);
-		param.put("offset", offset);
-		List<ShipmentDTO>list = dao.shipmentList(param);
-		int total = dao.shipmentListTotalPage(param);
 		Map<String, Object>result = new HashMap<String, Object>();
+		if(param.get("page") != null) {
+			int cnt = 10;
+			int offset = ((int)param.get("page")-1)*cnt;
+			param.put("cnt", cnt);
+			param.put("offset", offset);
+			int total = dao.shipmentListTotalPage(param);
+			result.put("total", total);
+		}
+		List<Map<String, Object>>list = dao.shipmentList(param);
 		result.put("list", list);
-		result.put("total", total);
-		result.put("page", param.get("param"));
 		return result;
 	}
 
 	public boolean shipmentDel(int idx) {
 		return dao.shipmentDel(idx)>0;
+	}
+
+	public Map<String, Object> shipmentProductList(int idx) {
+		List<Map<String, Object>>list =dao.shipmentProductList(idx); 
+		Map<String, Object>result = new HashMap<String, Object>();
+		result.put("list", list);
+		return result;
+	}
+
+	public Map<String, Object> shipmentProductStockList(Map<String, Object> param) {
+		List<Map<String, Object>>list = dao.shipmentProductStockList(param);
+		Map<String, Object>result = new HashMap<String, Object>();
+		result.put("list", list);
+		return result;
 	}
 	
 	
