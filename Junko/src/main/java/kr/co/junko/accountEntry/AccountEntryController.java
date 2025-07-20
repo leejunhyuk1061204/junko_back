83c3a6c74c54b8ff0d67196dc8309ed1969dc0f6
kim_kyoung_whoe <kw72622@naver.com>
2025-07-21 00:23:04 +0900

    입금 / 지급 관리 1. 상세보기 pdf 우측으로 보게 - 완료 2. 삭제 안됨 로그인 정보 없음 - 해결 3. Pdf 생성 실패, 등록 완료 메세지는 뜨나 리스트에 반영 안됨 4. 페이징처리 1페이지만 보임 - 완료 5. 수정 하기가 없음 - 수정 완료

--- Junko/src/main/java/kr/co/junko/accountEntry/AccountEntryController.java
+++ Junko/src/main/java/kr/co/junko/accountEntry/AccountEntryController.java
@@ -200,18 +200,16 @@ public class AccountEntryController {
 	public ResponseEntity<?> accountStatusUpdate(
 	    @PathVariable int entry_idx,
 	    @RequestBody Map<String, String> map,
-	    HttpServletRequest request
+	    @RequestHeader("user_idx") int user_idx
 	) {
-	    Integer user_idx = Jwt.getUserIdx(request);
-
 	    if (user_idx == 0) {
 	        return ResponseEntity.status(401).body(Map.of("success", false, "message", "로그인 필요"));
 	    }
 
 	    String newStatus = map.get("status");
 	    String logMsg = map.getOrDefault("logMsg", null);
-	    
-	 //  분개 유무 체크
+
+	    // 분개 유무 체크
 	    if (!service.hasDept(entry_idx)) {
 	        return ResponseEntity.badRequest().body(Map.of(
 	            "success", false,
@@ -219,14 +217,17 @@ public class AccountEntryController {
 	        ));
 	    }
 
-	    // 차/대변 검증 추가
+	    // 차/대변 검증
 	    if (!service.isBalanced(entry_idx)) {
 	        return ResponseEntity.badRequest().body(Map.of(
 	            "success", false,
 	            "message", "차변과 대변의 금액이 일치하지 않아 상태 변경이 불가능합니다."
 	        ));
 	    }
+	    log.info("🟢 상태 변경 요청 - entry_idx: {}, user_idx: {}", entry_idx, user_idx);
 	    
+	    log.info("⚖️ 차/대변 일치 여부: {}", service.isBalanced(entry_idx));
+
 	    service.accountStatusUpdate(entry_idx, newStatus, user_idx, logMsg);
 	    return ResponseEntity.ok().body(Map.of("success", true, "message", "상태 변경 완료!"));
 	}
